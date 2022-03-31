f3bc68d3b588767a5aa67087ca0ee8dc1e846f5b
Mike <mikhail-gankin@yandex.ru>
2022-03-31 20:02:02 +0300

    fonts

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+});
